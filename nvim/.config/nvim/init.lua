72eca8a81326dd3f3b80b179fe42ffca2f239e81
Marcin Wilk <marcin.wilk.dev@gmail.com>
2021-12-26 11:01:09 +0100

    nvim cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,23 +7,12 @@ require('config.telescope')
 
 require('config.lsp')
 require('config.cmp')
-require('config.db')
--- require('config.snippets')
+require('config.tree')
 
-require('lsp.java')
-require('lsp.lua')
 require('lsp.rust')
 require('lsp.bash')
 
 require('themes.gruvbox')
 require('themes.airline')
 
-require('config.jdtls_ui')
 require('config.commentary')
-
-vim.cmd([[
-    augroup jdtls_lsp
-        autocmd!
-        autocmd FileType java lua require('lsp.java').setup()
-    augroup end
-]])
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -2,3 +2,4 @@ if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
 	exec startx
 fi
 eval "$(/opt/homebrew/bin/brew shellenv)"
+export PATH=$PATH:/opt/homebrew/opt/llvm/bin
