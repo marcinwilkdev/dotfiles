775e37e6d5d9655b9bb76d5b976bba7213822d68
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-06-27 21:42:36 +0200

    add lualine which doesnt work :(

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
     use 'lewis6991/impatient.nvim'
     use 'goolord/alpha-nvim'
     use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
+    use "nvim-lualine/lualine.nvim"
 
     -- Colorscheme
     use "morhetz/gruvbox"
