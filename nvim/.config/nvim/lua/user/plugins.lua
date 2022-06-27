5223e90cd54ab274d209d157fa380cda5947bfaf
Marcin Wilk <marcin.wilk.dev@gmail.com>
2022-06-27 20:59:35 +0200

    add project plugin to nvim

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -51,6 +51,7 @@ return packer.startup(function(use)
     use "akinsho/bufferline.nvim"
     use "moll/vim-bbye"
     use "akinsho/toggleterm.nvim"
+    use "ahmedkhalf/project.nvim"
 
     -- Colorscheme
     use "morhetz/gruvbox"
