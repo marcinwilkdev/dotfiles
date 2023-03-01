f4c03d87600971fbefe19c11b92ae07a57e90875
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-03-01 20:24:02 +0100

    Lean config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,37 +1,4 @@
-lvim.log.level = "warn"
-lvim.format_on_save = false
 lvim.builtin.project.manual_mode = true
-
-lvim.leader = "space"
 
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-
-lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.terminal.active = true
-lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-lvim.builtin.treesitter.highlight.enabled = true
-lvim.builtin.treesitter.ensure_installed = {
-	"bash",
-	"c",
-	"cpp",
-	"javascript",
-	"java",
-	"json",
-	"lua",
-	"python",
-	"typescript",
-	"tsx",
-	"css",
-	"rust",
-	"java",
-	"yaml",
-}
-
-require("user.plugins")
-require("user.leap")
-require("user.lsp")
-require("user.neoscroll")
-require("user.treesitter")
