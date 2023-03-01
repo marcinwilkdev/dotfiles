694834be54af7d219591261988d01f414432e008
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-03-01 20:38:09 +0100

    Leaner lvim config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,4 +1,8 @@
-lvim.builtin.project.manual_mode = true
-
+vim.opt.relativenumber = true
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.builtin.treesitter.auto_install = true
+
+require ("lvim.lsp.null-ls.linters").setup {
+  { command = "flake8", filetypes = { "python" } },
+}
