fa97c76f24720d3df32fc5ea79fca992c536d90c
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-06-06 16:28:40 +0200

    Move lvim config to nvim

--- lvim/.config/lvim/config.lua
+++
@@ -1,3 +0,0 @@
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-vim.opt.relativenumber = true
---
+++ nvim/.config/nvim/lua/user/plenary.lua
@@ -0,0 +1,6 @@
+local M = {
+  "nvim-lua/plenary.nvim",
+  commit = "499e074",
+}
+
+return M
