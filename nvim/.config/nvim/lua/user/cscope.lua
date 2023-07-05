5358c107d8f7244c7704e3172696690bf897cc25
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-05 20:22:14 +0200

    Add cscope for nvim

---
+++ nvim/.config/nvim/lua/user/cscope.lua
@@ -0,0 +1,16 @@
+M = {
+  "dhananjaylatkar/cscope_maps.nvim",
+  commit = "46b22d441bd9aae0d46c7ed2d250e5c79d7e8be8",
+  event = "VimEnter",
+}
+
+M.opts = {
+  disable_maps = true,
+  skip_input_prompt = true,
+
+  cscope = {
+    skip_picker_for_single_result = true,
+  }
+}
+
+return M
