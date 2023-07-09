a06c8bdf4db79fe434c4b90b733af49aadb2b092
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-09 20:09:56 +0200

    Gruvbox

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,11 +1,13 @@
 local M = {
-  "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  "ellisonleao/gruvbox.nvim",
+  tag = "1.0.0",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-M.name = "tokyonight-night"
+vim.o.background = "dark"
+
+M.name = "gruvbox"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
