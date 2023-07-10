4a8c3910f38938231be9e05cb279b5771bb00915
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 23:11:41 +0200

    Pin half of nvim plugins

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,7 @@
 local M = {
   "ellisonleao/gruvbox.nvim",
   name = "gruvbox",
-  tag = "1.0.0",
+  commit = "df149bccb19a02c5c2b9fa6ec0716f0c0487feb0",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   pin = true,
