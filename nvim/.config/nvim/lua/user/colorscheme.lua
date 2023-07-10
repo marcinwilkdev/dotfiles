242d0e227478a0f6b32cf7379c7312971d97f398
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 23:04:59 +0200

    Cleaner gruvbox config

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,13 +1,14 @@
 local M = {
   "ellisonleao/gruvbox.nvim",
+  name = "gruvbox",
   tag = "1.0.0",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
+  pin = true,
 }
 
 vim.o.background = "dark"
 
-M.name = "gruvbox"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
