527996704a65cf16b44095962a791d0dae5f1b44
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 09:34:22 +0200

    Tidy up lsp

--- nvim/.config/nvim/lua/user/null-ls.lua
+++ nvim/.config/nvim/lua/user/null-ls.lua
@@ -22,13 +22,7 @@ function M.config()
   null_ls.setup {
     debug = false,
     sources = {
-      formatting.prettier.with {
-        extra_filetypes = { "toml" },
-        extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      },
       formatting.black.with { extra_args = { "--fast" } },
-      formatting.stylua,
-      formatting.google_java_format,
       diagnostics.flake8,
     },
   }
--- nvim/.config/nvim/lua/utils/init.lua
+++ nvim/.config/nvim/lua/utils/init.lua
@@ -2,13 +2,8 @@ local M = {}
 
 M.servers = {
   "lua_ls",
-  "cssls",
-  "html",
-  "tsserver",
   "pyright",
   "bashls",
-  "jsonls",
-  "yamlls",
 }
 
 return M
