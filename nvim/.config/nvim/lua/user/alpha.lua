615c3ca5a573a266c9b78da4e5af9795a5030bf3
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 23:04:20 +0200

    Cleaner alpha config

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -2,11 +2,16 @@ local M = {
   "goolord/alpha-nvim",
   event = "VimEnter",
   commit = "dafa11a6218c2296df044e00f88d9187222ba6b0",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  pin = true,
 }
 
 function M.config()
-  local alpha = require "alpha"
-  local dashboard = require "alpha.themes.dashboard"
+  local alpha = require("alpha")
+  local dashboard = require("alpha.themes.dashboard")
+
   dashboard.section.header.val = {
     [[                               __                ]],
     [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -15,20 +20,18 @@ function M.config()
     [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
   }
+
   dashboard.section.buttons.val = {
     dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-    dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
     dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
     dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
     dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
   }
-  local function footer()
-    return "Neovim"
-  end
 
-  dashboard.section.footer.val = footer()
+  dashboard.section.footer.val = "Neovim"
 
   dashboard.section.footer.opts.hl = "Type"
   dashboard.section.header.opts.hl = "Include"
