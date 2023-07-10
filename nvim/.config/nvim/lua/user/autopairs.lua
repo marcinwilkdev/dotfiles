dab682df72f15535d84454c2faf4fb99bb96291b
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 23:06:19 +0200

    Cleaner autopairs config

--- nvim/.config/nvim/lua/user/autopairs.lua
+++ nvim/.config/nvim/lua/user/autopairs.lua
@@ -2,44 +2,32 @@ local M = {
   "windwp/nvim-autopairs",
   commit = "0e065d423f9cf649e1d92443c939a4b5073b6768",
   event = "InsertEnter",
-  dependencies = {
-    {
-      "hrsh7th/nvim-cmp",
-      commit = "cfafe0a1ca8933f7b7968a287d39904156f2c57d",
-      event = {
-        "InsertEnter",
-        "CmdlineEnter",
-      },
-    },
+  dependencies = { "hrsh7th/nvim-cmp" },
+  pin = true,
+}
+
+M.opts = {
+  check_ts = true,   -- treesitter integration
+  disable_filetype = { "TelescopePrompt" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    offset = 0,   -- Offset from pattern match
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "PmenuSel",
+    highlight_grey = "LineNr",
   },
 }
 
 function M.config()
-  require("nvim-autopairs").setup {
-    check_ts = true, -- treesitter integration
-    disable_filetype = { "TelescopePrompt" },
-    ts_config = {
-      lua = { "string", "source" },
-      javascript = { "string", "template_string" },
-      java = false,
-    },
-    fast_wrap = {
-      map = "<M-e>",
-      chars = { "{", "[", "(", '"', "'" },
-      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-      offset = 0, -- Offset from pattern match
-      end_key = "$",
-      keys = "qwertyuiopzxcvbnmasdfghjkl",
-      check_comma = true,
-      highlight = "PmenuSel",
-      highlight_grey = "LineNr",
-    },
-  }
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-  local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-  local cmp = require "cmp"
+  require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done {})
 
-  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
+  require("nvim-autopairs").setup(M.opts)
 end
 
 return M
