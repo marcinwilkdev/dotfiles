d55bea72a9b1f16a72afa5a4abf37730f69b3deb
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-07-10 09:27:22 +0200

    Extrace cscope mappings

--- nvim/.config/nvim/lua/usercommands.lua
+++ nvim/.config/nvim/lua/usercommands.lua
@@ -1,13 +1,4 @@
-vim.api.nvim_create_user_command('Binds',
-  function ()
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_buf_set_keymap
-    local bufnr = vim.api.nvim_get_current_buf()
-    keymap(bufnr, "n", "gc", "<cmd>lua require('cscope_maps').cscope_prompt('c', vim.fn.expand('<cword>'))<CR>", opts)
-    keymap(bufnr, "n", "gd", "<cmd>lua require('cscope_maps').cscope_prompt('g', vim.fn.expand('<cword>'))<CR>", opts)
-    keymap(bufnr, "n", "gf", "<cmd>lua require('cscope_maps').cscope_prompt('f', vim.fn.expand('<cword>'))<CR>", opts)
-    keymap(bufnr, "n", "gr", "<cmd>lua require('cscope_maps').cscope_prompt('s', vim.fn.expand('<cword>'))<CR>", opts)
-    keymap(bufnr, "n", "gt", "<cmd>lua require('cscope_maps').cscope_prompt('t', vim.fn.expand('<cword>'))<CR>", opts)
-  end,
+vim.api.nvim_create_user_command("Binds",
+  require("helpers").create_cscope_key_mappings,
   {}
 )
