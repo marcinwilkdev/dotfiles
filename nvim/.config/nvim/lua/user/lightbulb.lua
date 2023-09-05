bf2ba1763a381eb5a409755a98db736683311604
Marcin Wilk <marcin.wilk.dev@gmail.com>
2023-09-05 11:27:14 +0200

    Add lightbulb

---
+++ nvim/.config/nvim/lua/user/lightbulb.lua
@@ -0,0 +1,14 @@
+local M = {
+  "kosayoda/nvim-lightbulb",
+  commit = "8f00b89dd1b1dbde16872bee5fbcee2e58c9b8e9",
+  lazy = false,
+  pin = true,
+}
+
+M.opts = {
+  autocmd = {
+    enabled = true
+  }
+}
+
+return M
